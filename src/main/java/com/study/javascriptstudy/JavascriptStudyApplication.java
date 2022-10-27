45a64b7d7bd94bd6ea457f6e5bf2927090522b7a
jisoo <q33asq33as@gmail.com>
2022-10-27 22:48:43 +0900

    자바스크립트 스킬업

---
+++ src/main/java/com/study/javascriptstudy/JavascriptStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.javascriptstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptStudyApplication.class, args);
+    }
+
+}
